1da0434f4543662a013a36d46517444d968ffd9c
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2019-03-15 20:59:34 +0100

    Menu for desktop + bgc desktop

--- js/toggle-snippet.js
+++ js/toggle-snippet.js
@@ -1,20 +1,37 @@
 (function () {
     var $btns = document.querySelectorAll('[data-toggle]');
-    var i = 0;
-    var btnsLength = $btns.length;
-    var onBtnClick = function () {
-        var targetSelector = this.getAttribute('data-toggle');
-        var $targets = document.querySelectorAll(targetSelector);
-        var toggleClass = this.getAttribute('data-toggle-class')||'on';
+    var each = function ($targets, modifier) {
         var i = 0;
         var targetsLength = $targets.length;
 
         for (i = 0; i < targetsLength; i++) {
-            $targets[i].classList.toggle(toggleClass);
+            modifier($targets[i]);
         }
     };
+    var onBtnClick = function () {
+        var targetSelector = this.getAttribute('data-toggle');
+        var hideBeforeSelector = this.getAttribute('data-toggle-hide-before');
+        var $targets = document.querySelectorAll(targetSelector);
+        var toggleClass = this.getAttribute('data-toggle-class')||'on';
 
-    for (i = 0; i <btnsLength; i++) {
-        $btns[i].addEventListener('click', onBtnClick, false);
+        if (hideBeforeSelector) {
+            var $hideBeforeTargets = document.querySelectorAll(hideBeforeSelector);
+
+            each($hideBeforeTargets, function ($el) {
+                $el.classList.remove(toggleClass);
+            });
+        }
+
+        each($targets, function ($el) {
+            $el.classList.toggle(toggleClass);
+        });
+    };
+
+    each($btns, function ($btn) {
+        $btn.addEventListener('click', onBtnClick, false);
+    });
+
+    for (i = 0; i < btnsLength; i++) {
+        
     }
 })();
